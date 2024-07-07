c6348b610ab5cb97340d19c8b425cb227e659dea
Chukwuma Onyeije <onyeije@gmail.com>
2024-07-06 20:40:17 -0400

    Updates

---
+++ precache-manifest.671f96780ae05d117e38618a1ccac342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be167f797d240ecaa0b8733757536821",
+    "url": "/kick-counter-monitor/index.html"
+  },
+  {
+    "revision": "b9693d7ecabea79c0e33",
+    "url": "/kick-counter-monitor/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/kick-counter-monitor/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/kick-counter-monitor/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "b9693d7ecabea79c0e33",
+    "url": "/kick-counter-monitor/static/js/main.8961209e.chunk.js"
+  },
+  {
+    "revision": "126d9a7c5a069253eb2e",
+    "url": "/kick-counter-monitor/static/js/runtime~main.063962c7.js"
+  }
+]);
